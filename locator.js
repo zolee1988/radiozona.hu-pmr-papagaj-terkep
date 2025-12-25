7de03bae9e10f9f5e52f5fa647f0a4181e70818e
Kiss Zoltán <zolee1988@gmail.com>
2025-12-25 09:51:11 +0100

    Update locator.js

--- locator.js
+++ locator.js
@@ -1,17 +1,21 @@
 function locatorToLatLng(locator) {
-    locator = locator.toUpperCase();
+    locator = locator.trim().toUpperCase();
 
     const A = 'A'.charCodeAt(0);
-    const a = 'a'.charCodeAt(0);
+    const a = 'A'.charCodeAt(0);
     const zero = '0'.charCodeAt(0);
 
     const lon = (locator.charCodeAt(0) - A) * 20 +
                 (locator.charCodeAt(2) - zero) * 2 +
-                (locator.charCodeAt(4) - a) / 12 - 180 + 1/12;
+                (locator.charCodeAt(4) - a) / 12 - 180;
 
     const lat = (locator.charCodeAt(1) - A) * 10 +
                 (locator.charCodeAt(3) - zero) +
-                (locator.charCodeAt(5) - a) / 24 - 90 + 1/24;
+                (locator.charCodeAt(5) - a) / 24 - 90;
 
-    return { lat, lng: lon };
+    // cella közepére igazítás
+    return {
+        lat: lat + 1 / 24,
+        lng: lon + 1 / 12
+    };
 }
