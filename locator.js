acc5f97d4d55146cdbdaa674979b7deb1d700406
Kiss Zoltán <zolee1988@gmail.com>
2025-12-25 09:47:37 +0100

    Update locator.js

--- locator.js
+++ locator.js
@@ -1,19 +1,17 @@
-function locatorToLatLon(locator) {
-  if (!locator || locator.length < 4) return { lat: 0, lon: 0 };
+function locatorToLatLng(locator) {
+    locator = locator.toUpperCase();
 
-  locator = locator.toUpperCase();
+    const A = 'A'.charCodeAt(0);
+    const a = 'a'.charCodeAt(0);
+    const zero = '0'.charCodeAt(0);
 
-  const A = 'A'.charCodeAt(0);
+    const lon = (locator.charCodeAt(0) - A) * 20 +
+                (locator.charCodeAt(2) - zero) * 2 +
+                (locator.charCodeAt(4) - a) / 12 - 180 + 1/12;
 
-  const lon = (locator.charCodeAt(0) - A) * 20
-            + (locator.charCodeAt(2) - 48) * 2
-            + (locator.charCodeAt(4) - A) / 12
-            - 180 + 1/12;
+    const lat = (locator.charCodeAt(1) - A) * 10 +
+                (locator.charCodeAt(3) - zero) +
+                (locator.charCodeAt(5) - a) / 24 - 90 + 1/24;
 
-  const lat = (locator.charCodeAt(1) - A) * 10
-            + (locator.charCodeAt(3) - 48)
-            + (locator.charCodeAt(5) - A) / 24
-            - 90 + 1/24;
-
-  return { lat, lon };
+    return { lat, lng: lon };
 }
