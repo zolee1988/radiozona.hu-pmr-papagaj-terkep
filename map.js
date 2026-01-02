64de8a4d654c08bfdc471c0cd45382b788fbebdc
Kiss Zoltán <zolee1988@gmail.com>
2026-01-02 08:19:50 +0100

    Update map.js

--- map.js
+++ map.js
@@ -9,8 +9,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // PMR ikon
 const pmrIcon = L.icon({
     iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
 });
+
 
 
 // JSON betöltése
